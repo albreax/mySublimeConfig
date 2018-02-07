ae32af38dff331e68ff552728a411bdea940a28f
Albrecht Naumann <albrecht.naumann@unitedprint.com>
2018-02-07 15:12:42 +0100

    update package

---
+++ Packages/SublimeLinter-contrib-perlcritic/linter.py
@@ -0,0 +1,36 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Gregory Oschwald (based on linter by Aparajita Fishman)
+# Copyright (c) 2013 Gregory Oschwald, Aparajita Fishman
+#
+# License: MIT
+#
+
+"""This module exports the PerlCritic linter class."""
+
+import os
+from SublimeLinter.lint import Linter, util
+
+
+class PerlCritic(Linter):
+
+    """Provides an interface to perlcritic."""
+
+    syntax = ('modernperl', 'perl')
+    executable = 'perlcritic'
+    regex = r'\[.+\] (?P<message>.+?) at line (?P<line>\d+), column (?P<col>\d+).+?'
+
+    def cmd(self):
+        """Return a tuple with the command line to execute."""
+
+        command = [self.executable_path, '--verbose', '8']
+
+        config = util.find_file(
+            os.path.dirname(self.filename), '.perlcriticrc')
+
+        if config:
+            command += ['-p', config]
+
+        return command
